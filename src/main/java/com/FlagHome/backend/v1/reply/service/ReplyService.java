795721cca405fb75748e8dd28eb4813754dac900
gmltmd23 <dbsgmltmd23@naver.com>
2022-11-07 22:37:04 +0900

    [추가/개선] Create Reply 대응 (2022.11.07 윤희승)   - ReplyDto 추가   - Reply를 Create 할수 있게끔 대응   - 컨트롤러에 맞는 테스트 코드 추가

--- src/main/java/com/FlagHome/backend/v1/reply/service/ReplyService.java
+++ src/main/java/com/FlagHome/backend/v1/reply/service/ReplyService.java
@@ -1,7 +1,13 @@
 package com.FlagHome.backend.v1.reply.service;
 
+import com.FlagHome.backend.v1.Status;
+import com.FlagHome.backend.v1.post.entity.Post;
+import com.FlagHome.backend.v1.post.repository.PostRepository;
+import com.FlagHome.backend.v1.reply.dto.ReplyDto;
 import com.FlagHome.backend.v1.reply.entity.Reply;
 import com.FlagHome.backend.v1.reply.repository.ReplyRepository;
+import com.FlagHome.backend.v1.user.entity.User;
+import com.FlagHome.backend.v1.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +16,34 @@ import java.util.List;
 @Service
 public class ReplyService {
     @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private ReplyRepository replyRepository;
 
+    public ReplyDto createReply(ReplyDto replyDto) {
+        Post foundPost = postRepository.findById(replyDto.getPostId()).orElse(null);
+        User foundUser = userRepository.findById(replyDto.getUserId()).orElse(null);
+
+        Reply replyEntity = Reply.builder()
+                                .user(foundUser)
+                                .post(foundPost)
+                                .content(replyDto.getContent())
+                                .replyGroup(replyDto.getReplyGroup())
+                                .replyOrder(replyDto.getReplyOrder())
+                                .replyDepth(replyDto.getReplyDepth())
+                                .status(Status.ON)
+                                .build();
+        replyRepository.save(replyEntity);
+
+        replyDto.setId(replyEntity.getId());
+        return replyDto;
+    }
+
     public List<Reply> findReplies(Long postId) {
-        return  replyRepository.findByPostId(postId);
+        return replyRepository.findByPostId(postId);
     }
 }
