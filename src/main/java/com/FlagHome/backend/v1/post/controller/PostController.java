2f84a99996d22b5aeefc82962f43565fd6aa4ade
gmltmd23 <dbsgmltmd23@naver.com>
2022-11-15 21:40:17 +0900

    [추가] PostController 생성 (2022.11.15 윤희승)

---
+++ src/main/java/com/FlagHome/backend/v1/post/controller/PostController.java
@@ -0,0 +1,10 @@
+package com.FlagHome.backend.v1.post.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/post")
+public class PostController {
+
+}
