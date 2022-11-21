7754f6fc61f07c6695e97df38ab72acf880f3eab
gogun <rjs0219ko@gmail.com>
2022-11-21 22:38:40 +0900

    [추가] GET POST(2022.11.21 고건)

--- src/main/java/com/FlagHome/backend/v1/post/controller/PostController.java
+++ src/main/java/com/FlagHome/backend/v1/post/controller/PostController.java
@@ -4,10 +4,7 @@ import com.FlagHome.backend.v1.post.dto.PostDto;
 import com.FlagHome.backend.v1.post.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/post")
@@ -19,6 +16,18 @@ public class PostController {
     @PutMapping("/update")
     public ResponseEntity<Void> updatePost(@RequestBody PostDto postDto) {
         postService.updatePost(postDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/post")
+    public ResponseEntity<Void> getPost(@RequestBody PostDto postDto) {
+        postService.createPost(postDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/post/{id}")
+    public ResponseEntity<Long> putPost(@RequestBody PostDto postDto) {
+        postService.createPost(postDto);//여기 분명 코드를 넣어야 하는데 뭘 넣어야 할지 모르겠어요ㅠㅠ...
         return ResponseEntity.ok().build();
     }
 }
