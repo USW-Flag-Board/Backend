c15d5e5cf6751ad72e85953cb957aab297192932
gmltmd23 <dbsgmltmd23@naver.com>
2022-11-29 20:55:07 +0900

    [개선] 빌드오류 발생 부분 해결 (2022.11.29 윤희승)  - master branch에는 반드시 프로젝트 전체 빌드 테스트 하고 올려주세요 ㅠㅠ

--- src/main/java/com/FlagHome/backend/v1/token/entity/RefreshToken.java
+++ src/main/java/com/FlagHome/backend/v1/token/entity/RefreshToken.java
@@ -2,12 +2,10 @@ package com.FlagHome.backend.v1.token.entity;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 @Getter
 @Builder
 @NoArgsConstructor
