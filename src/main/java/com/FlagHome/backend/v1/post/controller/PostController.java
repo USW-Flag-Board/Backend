3c2f7b412fa5e9753a3d5b8324216b0431e15c66
gmltmd23 <dbsgmltmd23@naver.com>
2022-11-29 22:03:58 +0900

    [추가/개선] Post CRUD 완료 및 코드 개선 (2022.11.29 윤희승)

--- src/main/java/com/FlagHome/backend/v1/post/controller/PostController.java
+++ src/main/java/com/FlagHome/backend/v1/post/controller/PostController.java
@@ -2,26 +2,37 @@ package com.FlagHome.backend.v1.post.controller;
 
 import com.FlagHome.backend.v1.post.dto.PostDto;
 import com.FlagHome.backend.v1.post.service.PostService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/post")
+@RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
 
-    @Autowired
-    public PostController(PostService postService) {
-        this.postService = postService;
+    @PostMapping("/create")
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+        postService.createPost(postDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<PostDto> getPost(@RequestParam(value = "postId") long postId) {
+        return ResponseEntity.status(HttpStatus.OK).body(postService.getPost(postId));
     }
 
     @PutMapping("/update")
     public ResponseEntity<Void> updatePost(@RequestBody PostDto postDto) {
         postService.updatePost(postDto);
         return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/delete/{post_id}")
+    public ResponseEntity<Void> deletePost(@PathVariable(name = "post_id") long postId) {
+        postService.deletePost(postId);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/FlagHome/backend/v1/post/service/PostService.java
+++ src/main/java/com/FlagHome/backend/v1/post/service/PostService.java
@@ -1,22 +1,48 @@
 package com.FlagHome.backend.v1.post.service;
 
+import com.FlagHome.backend.global.exception.CustomException;
+import com.FlagHome.backend.global.exception.ErrorCode;
+import com.FlagHome.backend.v1.member.entity.Member;
+import com.FlagHome.backend.v1.member.repository.MemberRepository;
 import com.FlagHome.backend.v1.post.dto.PostDto;
 import com.FlagHome.backend.v1.post.entity.Post;
 import com.FlagHome.backend.v1.post.repository.PostRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@RequiredArgsConstructor
 public class PostService {
+    private final MemberRepository memberRepository;
     private final PostRepository postRepository;
 
-    @Autowired
-    public PostService(PostRepository postRepository) {
-        this.postRepository = postRepository;
+    @Transactional
+    public void createPost(PostDto postDto) {
+        Member memberEntity = memberRepository.findById(postDto.getUserId()).orElse(null);
+        if(memberEntity == null)
+            throw new CustomException(ErrorCode.USER_NOT_EXISTS);
+
+        postRepository.save(Post.builder()
+                .member(memberEntity)
+                .title(postDto.getTitle())
+                .content(postDto.getContent())
+                .category(postDto.getCategory())
+                .status(postDto.getStatus())
+                .viewCount(0L)
+                .build());
     }
 
+    @Transactional
+    public PostDto getPost(long postId) {
+        Post postEntity = postRepository.findById(postId).orElse(null);
+        if(postEntity == null)
+            throw new CustomException(ErrorCode.POST_NOT_EXISTS);
+
+        return new PostDto(postEntity);
+    }
+
+    @Transactional
     public void updatePost(PostDto postDto) {
         Post postEntity = postRepository.findById(postDto.getId()).orElse(null);
         assert postEntity != null;
@@ -24,5 +50,10 @@ public class PostService {
         postEntity.setTitle(postDto.getTitle());
         postEntity.setContent(postDto.getContent());
         postEntity.setCategory(postDto.getCategory());
+    }
+
+    @Transactional
+    public void deletePost(long postId) {
+        postRepository.deleteById(postId);
     }
 }
