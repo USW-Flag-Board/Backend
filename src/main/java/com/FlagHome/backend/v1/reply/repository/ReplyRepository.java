1bd0c7d4ac20f3fd0f2eacf1ef970ebe5fe4177c
gmltmd23 <dbsgmltmd23@naver.com>
2022-11-29 22:03:58 +0900

    [추가/개선] Post CRUD 완료 및 코드 개선 (2022.11.29 윤희승)

--- src/main/java/com/FlagHome/backend/v1/reply/repository/ReplyRepository.java
+++ src/main/java/com/FlagHome/backend/v1/reply/repository/ReplyRepository.java
@@ -4,11 +4,7 @@ import com.FlagHome.backend.v1.reply.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-@Repository
-@Transactional(readOnly = true)
 public interface ReplyRepository extends JpaRepository<Reply, Long>, ReplyRepositoryCustom {
     @Modifying
     @Query(value = "truncate table reply", nativeQuery = true)
