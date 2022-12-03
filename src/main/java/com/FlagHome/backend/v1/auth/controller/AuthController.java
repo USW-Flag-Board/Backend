cd7c1005eae1894f47e0d13f9541242c7e0b43ba
김도현 <kitten.diger@gmail.com>
2022-12-03 21:19:32 +0900

    pullling project By K-Diger (#23)

--- src/main/java/com/FlagHome/backend/v1/auth/controller/AuthController.java
+++ src/main/java/com/FlagHome/backend/v1/auth/controller/AuthController.java
@@ -1,6 +1,6 @@
 package com.FlagHome.backend.v1.auth.controller;
 
-import com.FlagHome.backend.v1.auth.dto.LogInRequest;
+import com.FlagHome.backend.v1.auth.dto.LoginRequest;
 import com.FlagHome.backend.v1.auth.dto.SignUpRequest;
 import com.FlagHome.backend.v1.auth.dto.SignUpResponse;
 import com.FlagHome.backend.v1.auth.service.AuthService;
@@ -9,7 +9,6 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +27,7 @@ public class AuthController {
 
     @PostMapping("/login")
     @ApiOperation(value = "로그인")
-    public ResponseEntity<TokenResponse> logIn(@RequestBody LogInRequest logInRequest) {
-        return ResponseEntity.ok(authService.logIn(logInRequest));
+    public ResponseEntity<TokenResponse> logIn(@RequestBody LoginRequest logInRequest) {
+        return ResponseEntity.ok(authService.login(logInRequest));
     }
 }
