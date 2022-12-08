f806bd9ea36b3727a3d764afbfd15709a89578ea
He Jow Moon <81742959+Hejow@users.noreply.github.com>
2022-12-09 02:50:03 +0900

    [추가, 개선] : Aws S3, Security Util 추가, Reissue Token 개선 (2022.12.09 문희조) (#26)
    [Aws S3]
     - configuration 추가
     - 파일 업로드 유틸 추가
    
    [Member]
     - issueMember 구현했으나 삭제 (불필요)
    
    [JWT]
     - 토큰 재발급 코드 개선

--- src/main/java/com/FlagHome/backend/global/exception/ErrorCode.java
+++ src/main/java/com/FlagHome/backend/global/exception/ErrorCode.java
@@ -1,5 +1,6 @@
 package com.FlagHome.backend.global.exception;
 
+import io.swagger.models.auth.In;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
@@ -24,11 +25,11 @@ public enum ErrorCode {
     LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요."),
     UNAUTHORIZED_TOKEN(HttpStatus.UNAUTHORIZED, "권한이 없는 토큰입니다."),
 
-    /** 403 - FORBIDDEN */
-    INVALID_RESTRICTED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
-
     /** 404 - NOT_FOUND */
     INVALID_FOUND(HttpStatus.NOT_FOUND, "요청을 찾을 수 없습니다."),
+
+    /** 500 - INTERNAL_SERVER_ERROR */
+    FILE_CONVERT_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "파일 변환에 실패했습니다.")
     ;
 
     private final HttpStatus httpStatus;
---
+++ src/main/java/com/FlagHome/backend/v1/util/SecurePasswordGenerator.java
@@ -0,0 +1,12 @@
+package com.FlagHome.backend.v1.util;
+
+import java.security.SecureRandom;
+
+public class SecurePasswordGenerator {
+
+    public static String generate() {
+
+        SecureRandom random = new SecureRandom();
+        return null;
+    }
+}
