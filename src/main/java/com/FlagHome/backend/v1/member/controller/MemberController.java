acf68fce9d2c239df220c3f9dc4c95c2df55b0e0
yabueng0219 <rjs0219ko@gmail.com>
2022-12-15 05:02:20 +0900

    유저DTO 삭제

--- src/main/java/com/FlagHome/backend/v1/member/controller/MemberController.java
+++ src/main/java/com/FlagHome/backend/v1/member/controller/MemberController.java
@@ -1,9 +1,7 @@
 package com.FlagHome.backend.v1.member.controller;
 
 import com.FlagHome.backend.v1.member.service.MemberService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
--- src/main/java/com/FlagHome/backend/v1/member/service/MemberService.java
+++ src/main/java/com/FlagHome/backend/v1/member/service/MemberService.java
@@ -1,12 +1,19 @@
 package com.FlagHome.backend.v1.member.service;
 
+import com.FlagHome.backend.global.exception.CustomException;
+import com.FlagHome.backend.global.exception.ErrorCode;
 import com.FlagHome.backend.v1.member.entity.Member;
 import com.FlagHome.backend.v1.member.repository.MemberRepository;
+import com.FlagHome.backend.v1.user.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -14,4 +21,15 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MemberService {
 
+    private final MemberRepository memberRepository;
+
+
+    @Transactional
+    public void updateMember(@NotNull UserDto userDto) {
+        Member memberEntity = MemberRepository.findByLoginId(userDto.getId());
+        .setPassword(userDto.getPassword());
+        .setName(userDto.getName());
+        .setPhoneNumber(userDto.getPhoneNumber());
+        .setMajor(userDto.getMajor());
+    }
 }
