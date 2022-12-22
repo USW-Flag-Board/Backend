730216282dc3697d98e0b3e1f9565fe88211702e
Moon He Jow <gmlwh124@naver.com>
2022-12-22 19:38:02 +0900

    [추가, 개선] 유효성 검사 추가, 컨트롤러 개선, 서비스 개선 (2022.12.22 문희조)  - 아이디, 이메일 유효성 검사 추가  - 비밀번호 수정 추가  - 비밀번호 재발급 로직 개선  - SecurityUtil 사용 계층 변경 (method -> controller)  - 에러 코드 추가(400) 및 개선(404)  - 메일 전송 관련 테스트 보류(SES 아트박스 해제 예정)

--- src/main/java/com/FlagHome/backend/v1/member/service/MemberService.java
+++ src/main/java/com/FlagHome/backend/v1/member/service/MemberService.java
@@ -2,12 +2,13 @@ package com.FlagHome.backend.v1.member.service;
 
 import com.FlagHome.backend.global.exception.CustomException;
 import com.FlagHome.backend.global.exception.ErrorCode;
+import com.FlagHome.backend.global.util.RandomNumberGenerator;
+import com.FlagHome.backend.v1.Status;
+import com.FlagHome.backend.v1.member.dto.UpdatePasswordRequest;
 import com.FlagHome.backend.v1.member.entity.Member;
 import com.FlagHome.backend.v1.member.mail.MailService;
 import com.FlagHome.backend.v1.member.mail.MailType;
 import com.FlagHome.backend.v1.member.repository.MemberRepository;
-import com.FlagHome.backend.global.util.RandomNumberGenerator;
-import com.FlagHome.backend.global.util.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,43 +24,57 @@ public class MemberService {
 
     private final MailService mailService;
 
-    @Transactional
     public void validateDuplicateLoginId(String loginId) {
         if (memberRepository.existsByLoginId(loginId)) {
-            throw new CustomException(ErrorCode.USER_ID_EXISTS);
+            throw new CustomException(ErrorCode.LOGIN_ID_EXISTS);
         }
     }
 
-    @Transactional
     public void validateEmail(String email) {
         validateUSWEmail(email);
 
         if (memberRepository.existsByEmail(email)) {
-            throw new CustomException(ErrorCode.USER_ID_EXISTS);
+            throw new CustomException(ErrorCode.EMAIL_EXISTS);
         }
     }
 
     @Transactional
-    public void withdrawMember(String password) {
-        Member member = memberRepository.findById(SecurityUtils.getMemberId()).get();
+    public void withdraw(Long memberId, String password) {
+        validatePassword(memberId, password);
 
-        if (!passwordEncoder.matches(password, member.getPassword())) {
-            throw new CustomException(ErrorCode.PASSWORD_NOT_MATCH);
-        }
-
-        memberRepository.withdraw(SecurityUtils.getMemberId());
+        Member member = memberRepository.findById(memberId).get();
+        // dirty checking
+        member.updateStatus(Status.WITHDRAW);
     }
 
     @Transactional
-    public void sendFindIdResult(String name, String email) {
-        Member member = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_EXISTS));
+    public void findLoginId(String name, String email) {
+        Member member = findMemberByEmail(email);
 
         if (!StringUtils.equals(member.getName(), name)) {
             throw new CustomException(ErrorCode.NAME_EMAIL_NOT_MATCH);
         }
 
-        mailService.sendMail(email, MailType.FIND_ID, member.getLoginId());
+        sendFindIdResult(member.getLoginId(), email);
+    }
+
+    @Transactional
+    public void sendFindIdResult(String loginId, String email) {
+        mailService.sendMail(email, MailType.FIND_ID, loginId);
+    }
+
+    @Transactional
+    public void reissuePassword(String loginId, String email) {
+        Member member = findMemberByEmail(email);
+
+        if (!StringUtils.equals(member.getLoginId(), loginId)) {
+            throw new CustomException(ErrorCode.ID_EMAIL_NOT_MATCH);
+        }
+
+        String newPassword = RandomNumberGenerator.getRandomPassword();
+        // dirty checking
+        member.updatePassword(passwordEncoder.encode(newPassword));
+        sendFindPasswordResult(email, newPassword);
     }
 
     @Transactional
@@ -68,18 +83,30 @@ public class MemberService {
     }
 
     @Transactional
-    public String updatePassword(String loginId, String email) {
-        Member member = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_EXISTS));
+    public void updatePassword(Long memberId, UpdatePasswordRequest updatePasswordRequest) {
+        validatePassword(memberId, updatePasswordRequest.getCurrentPassword());
 
-        if (!StringUtils.equals(member.getLoginId(), loginId)) {
-            throw new CustomException(ErrorCode.ID_EMAIL_NOT_MATCH);
+        Member member = memberRepository.findById(memberId).get();
+        if (passwordEncoder.matches(updatePasswordRequest.getNewPassword(), member.getPassword())) {
+            throw new CustomException(ErrorCode.PASSWORD_IS_SAME);
         }
 
-        String newPassword = RandomNumberGenerator.getRandomPassword();
-        memberRepository.updatePassword(member.getId(), newPassword);
+        // dirty checking
+        member.updatePassword(passwordEncoder.encode(updatePasswordRequest.getNewPassword()));
+    }
 
-        return newPassword;
+    private void validatePassword(Long memberId, String password) {
+        // 비밀번호 검사는 로그인된 대상만 진행
+        Member member = memberRepository.findById(memberId).get();
+
+        if (!passwordEncoder.matches(password, member.getPassword())) {
+            throw new CustomException(ErrorCode.PASSWORD_NOT_MATCH);
+        }
+    }
+
+    private Member findMemberByEmail(String email) {
+        return memberRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
     }
 
     private void validateUSWEmail(String email) {
