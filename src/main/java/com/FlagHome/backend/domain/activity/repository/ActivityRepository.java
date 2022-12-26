82384f9884a4acb70fd74c6a7026c0a6ce69b242
yabueng0219 <rjs0219ko@gmail.com>
2022-12-26 22:07:23 +0900

    임시커밋

--- src/main/java/com/FlagHome/backend/domain/activity/repository/ActivityRepository.java
+++ src/main/java/com/FlagHome/backend/domain/activity/repository/ActivityRepository.java
@@ -5,7 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Transactional
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long>, ActivityRepositoryCustom {
+
+    static void deleteById(Activity activityId) {
+    }
+
+    Optional<Activity> findByID(Long activityId);
 }
--- src/main/java/com/FlagHome/backend/domain/activity/repository/ActivityRepositoryImpl.java
+++ src/main/java/com/FlagHome/backend/domain/activity/repository/ActivityRepositoryImpl.java
@@ -1,12 +1,26 @@
 package com.FlagHome.backend.domain.activity.repository;
 
 
+import com.FlagHome.backend.domain.activity.entity.Activity;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import javax.transaction.Transactional;
+import java.util.List;
+
+import static com.FlagHome.backend.domain.activity.entity.QActivity.activity;
 
 @Repository
 @Transactional
 @RequiredArgsConstructor
 public class ActivityRepositoryImpl implements ActivityRepositoryCustom {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<Activity> findByActivityId(long activityId) {
+        return jpaQueryFactory
+                .selectFrom(activity)
+                .where(activity.id.eq(activityId))
+                .fetch();
+    }
 }
