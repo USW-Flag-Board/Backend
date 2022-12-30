0cc6fe3717102643b3ddd0a885f92ff8f520c319
sdoaolo <09jieun01@naver.com>
2022-12-31 03:11:32 +0900

    [추가,리팩토링] CategoryMapper(MapStruct)를 이용한 리팩토링 (2022.12.31 강지은)

--- src/main/java/com/FlagHome/backend/domain/category/service/CategoryService.java
+++ src/main/java/com/FlagHome/backend/domain/category/service/CategoryService.java
@@ -2,12 +2,9 @@ package com.FlagHome.backend.domain.category.service;
 
 import com.FlagHome.backend.global.exception.CustomException;
 import com.FlagHome.backend.global.exception.ErrorCode;
-import com.FlagHome.backend.domain.category.dto.CategoryDto;
 import com.FlagHome.backend.domain.category.entity.Category;
-import com.FlagHome.backend.domain.category.dto.CategoryResultDto;
 import com.FlagHome.backend.domain.category.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -20,58 +17,39 @@ import java.util.stream.Collectors;
 public class CategoryService {
     private final CategoryRepository categoryRepository;
     @Transactional
-    public void createCategory (CategoryDto categoryDto) {
-
-        Category parentCategory = null;
-
-        if(categoryDto.getCategoryDepth()!=0){
-            parentCategory = findVerifiedCategory(categoryDto.getParentId());
-        }
-
-        Category category = Category.builder()
-                .koreanName(categoryDto.getKoreanName())
-                .englishName(categoryDto.getEnglishName())
-                .parent(parentCategory)
-                .categoryDepth(categoryDto.getCategoryDepth())
-                .build();
+    public Category createCategory(Category category){
 
         categoryRepository.save(category);
+        Category parentCategory = category.getParent();
 
         if(parentCategory != null)
             parentCategory.getChildren().add(category);
 
+        return category;
     }
-
     @Transactional
-    public Category updateCategory (CategoryDto categoryDto) {
+    public Category updateCategory (Category category) {
 
-        Category category = findVerifiedCategory(categoryDto.getId());
+        Category findCategory = findVerifiedCategory(category.getId());
 
-
-        Optional.ofNullable(categoryDto.getKoreanName())
-                .ifPresent(koreanName -> category.setKoreanName(koreanName));
-        Optional.ofNullable(categoryDto.getEnglishName())
-                .ifPresent(englishName -> category.setEnglishName(englishName));
-        Optional.ofNullable(categoryDto.getCategoryDepth())
-                .ifPresent(depth -> category.setCategoryDepth(depth));
-        Optional.ofNullable(categoryDto.getParentId())
-                .ifPresent(parent -> category.setParent(
-                        findVerifiedCategory(categoryDto.getParentId())
+        Optional.ofNullable(category.getKoreanName())
+                .ifPresent(koreanName -> findCategory.setKoreanName(koreanName));
+        Optional.ofNullable(category.getEnglishName())
+                .ifPresent(englishName -> findCategory.setEnglishName(englishName));
+        Optional.ofNullable(category.getCategoryDepth())
+                .ifPresent(depth -> findCategory.setCategoryDepth(depth));
+        Optional.ofNullable(category.getParent())
+                .ifPresent(parent -> findCategory.setParent(
+                        findVerifiedCategory(category.getParent().getId())
                 ));
 
-        return categoryRepository.save(category);
+        return categoryRepository.save(findCategory);
 
     }
 
     @Transactional
-    public List<CategoryResultDto> getCategories () {
-
-        List<CategoryResultDto> categories = categoryRepository
-                .findAll().stream()
-                .map(CategoryResultDto::of)
-                .collect(Collectors.toList());
-
-        return categories;
+    public List<Category> getCategories () {
+        return categoryRepository.findAll();
     }
 
     @Transactional
@@ -80,7 +58,7 @@ public class CategoryService {
         categoryRepository.delete(category);
     }
 
-    private Category findVerifiedCategory(long categoryId) {
+    public Category findVerifiedCategory(long categoryId) {
         Optional<Category> optionalCategory = categoryRepository.findById(categoryId);
         Category findCategory = optionalCategory.orElseThrow(()-> new CustomException(ErrorCode.CATEGORY_NOT_EXISTS));
         return findCategory;
