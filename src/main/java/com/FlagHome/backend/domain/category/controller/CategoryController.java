4944120b2a4b060db542c37691237274d22bf08b
sdoaolo <09jieun01@naver.com>
2022-12-31 03:11:32 +0900

    [추가,리팩토링] CategoryMapper(MapStruct)를 이용한 리팩토링 (2022.12.31 강지은)

--- src/main/java/com/FlagHome/backend/domain/category/controller/CategoryController.java
+++ src/main/java/com/FlagHome/backend/domain/category/controller/CategoryController.java
@@ -1,11 +1,10 @@
 package com.FlagHome.backend.domain.category.controller;
 
 import com.FlagHome.backend.domain.category.dto.CategoryDto;
-import com.FlagHome.backend.domain.category.dto.CategoryResultDto;
-import com.FlagHome.backend.domain.category.entity.Category;
+import com.FlagHome.backend.domain.category.mapper.CategoryMapper;
 import com.FlagHome.backend.domain.category.service.CategoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,32 +15,31 @@ import org.springframework.web.bind.annotation.*;
 public class CategoryController {
 
     private final CategoryService categoryService;
+    private final CategoryMapper mapper;
 
     @PostMapping
     public ResponseEntity createCategory(@RequestBody CategoryDto categoryDto) {
-        categoryService.createCategory(categoryDto);
-        return new ResponseEntity(HttpStatus.CREATED); //BODY 던져주기!
+        categoryService.createCategory(mapper.CategoryDtoToCategory(categoryDto,categoryService));
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
-    @PatchMapping("/{categoryId}")
-    public ResponseEntity updateCategory(@PathVariable long categoryId,
+    @PatchMapping("/{category-id}")
+    public ResponseEntity updateCategory(@PathVariable("category-id") long categoryId,
                                          @RequestBody CategoryDto categoryDto) {
         categoryDto.setId(categoryId);
-        Category result =categoryService.updateCategory(categoryDto);
-        return new ResponseEntity(HttpStatus.OK);
+        categoryService.updateCategory(mapper.CategoryDtoToCategory(categoryDto,categoryService));
+        return ResponseEntity.ok().build();
     }
 
 
     @GetMapping
     public ResponseEntity<?> getCategories() {
-        return ResponseEntity.ok(categoryService.getCategories());
+        return ResponseEntity.ok(mapper.CategoryListToCategoryResultDtoList(categoryService.getCategories()));
     }
 
-    @DeleteMapping("/{categoryId}")
-    public ResponseEntity deleteCatgory(@PathVariable long categoryId) {
+    @DeleteMapping("/{category-id}")
+    public ResponseEntity deleteCatgory(@PathVariable("category-id") long categoryId) {
         categoryService.deleteCategory(categoryId);
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
+        return ResponseEntity.noContent().build();
     }
-
-
 }
