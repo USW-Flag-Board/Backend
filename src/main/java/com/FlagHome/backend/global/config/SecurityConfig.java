b3a64fc21e0f37383ed56f2a9f31194a940649fb
Moon He Jow <gmlwh124@naver.com>
2023-01-03 11:21:46 +0900

    [추가, 수정] WebSecurityCustomizer 추가, CI 수정 (2023.1.3 문희조)

--- src/main/java/com/FlagHome/backend/global/config/SecurityConfig.java
+++ src/main/java/com/FlagHome/backend/global/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,6 +26,12 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder encodPassword() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .antMatchers("/swagger-ui.html", "/swagger-ui/**");
     }
 
     @Bean
