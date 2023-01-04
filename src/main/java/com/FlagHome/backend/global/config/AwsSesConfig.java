cf0365ac965631d1d3e7a924297f637eddeac66f
Moon He Jow <gmlwh124@naver.com>
2023-01-04 17:32:19 +0900

    [추가, 수정] 테스트 설정 추가, CI/CD 플로우 개선 (2023.1.4 문희조)

--- src/main/java/com/FlagHome/backend/global/config/AwsSesConfig.java
+++ src/main/java/com/FlagHome/backend/global/config/AwsSesConfig.java
@@ -10,13 +10,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsSesConfig {
-    @Value("${cloud.ses.credentials.accessKey}")
+    @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
 
-    @Value("${cloud.ses.credentials.secretKey}")
+    @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
 
-    @Value("${cloud.ses.region.static}")
+    @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
