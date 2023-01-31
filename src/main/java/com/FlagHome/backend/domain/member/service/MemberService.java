d1dcbcc29689d5f63cfa686fd3cd8136a4c9043d
Moon He Jow <gmlwh124@naver.com>
2023-01-31 09:00:41 +0900

    [수정] 로그인 로그 가져오기 수정 (2023.1.31 문희조)

--- src/main/java/com/FlagHome/backend/domain/member/service/MemberService.java
+++ src/main/java/com/FlagHome/backend/domain/member/service/MemberService.java
@@ -9,9 +9,9 @@ import com.FlagHome.backend.domain.member.avatar.dto.MyProfileResponse;
 import com.FlagHome.backend.domain.member.avatar.dto.UpdateAvatarRequest;
 import com.FlagHome.backend.domain.member.avatar.service.AvatarService;
 import com.FlagHome.backend.domain.member.dto.FindResponse;
+import com.FlagHome.backend.domain.member.dto.LoginLogResponse;
 import com.FlagHome.backend.domain.member.dto.MemberProfileResponse;
 import com.FlagHome.backend.domain.member.dto.UpdatePasswordRequest;
-import com.FlagHome.backend.domain.member.dto.ViewLogResponse;
 import com.FlagHome.backend.domain.member.entity.Member;
 import com.FlagHome.backend.domain.member.repository.MemberRepository;
 import com.FlagHome.backend.domain.post.dto.PostDto;
@@ -174,7 +174,7 @@ public class MemberService {
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
     }
 
-    public List<ViewLogResponse> viewLog() {
-        return memberRepository.getAllLogs();
+    public List<LoginLogResponse> getAllLoginLogs() {
+        return memberRepository.getAllLoginLogs();
     }
 }
