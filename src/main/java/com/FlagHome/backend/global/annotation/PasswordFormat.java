a4a4fcca0a42b7f515301a5a87e3044f0e3d1849
Moon He Jow <gmlwh124@naver.com>
2023-03-18 19:06:30 +0900

    [추가] 커스텀 어노테이션 추가 (2023.03.18 문희조)  - InputValidator 유효성 분리를 위해 추가  - USW 이메일, 비밀번호 구현

---
+++ src/main/java/com/FlagHome/backend/global/annotation/PasswordFormat.java
@@ -0,0 +1,18 @@
+package com.FlagHome.backend.global.annotation;
+
+import javax.validation.Constraint;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordFormatValidator.class)
+public @interface PasswordFormat {
+    String message() default "사용할 수 없는 비밀번호 입니다. (8~20자 이내 영문, 숫자, 특수문자를 모두 포함)";
+
+    Class[] groups() default {};
+
+    Class[] payload() default {};
+}
